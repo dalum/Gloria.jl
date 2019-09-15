52c9c868664dcecc5bcd2768972839836e5bde19
F Freyer <frederic481994@hotmail.de>
2019-09-15 13:40:13 +0200

    mostly fix Inside (TODO inside(line, p))

--- examples/Inside/src/Inside.jl
+++ examples/Inside/src/Inside.jl
@@ -1,11 +1,15 @@
 module Inside
 
 import Gloria: onevent!, render!, update!
-using Gloria: Gloria, AbstractObject, AbstractShape, Audio, Circle, Event, Layer, Line, Lines, Point, Polygon, Scene, Texture, Window,
-    add!, kill!, play!,
-    getmousestate, inside, intersects, isalive, isbutton, iskey, ispressed, tocoordinates
+using Gloria: Gloria, AbstractObject, Audio, add!, kill!, play!, Scene, Texture,
+    Window, isalive, isbutton, iskey, ispressed, getmousestate, tocoordinates,
+    Event, Layer
+using Gloria.Shapes: AbstractShape, circle, Polyline, Vertex, vertices,
+    Polygon, intersects, inside, translate, rotate, centroid
 
 using Colors: RGB, @colorant_str
+using LinearAlgebra
+
 
 ##################################################
 # Types
@@ -43,8 +47,29 @@ function onevent!(self::Shape, e::Event{:mousemotion})
 end
 
 function onevent!(self::Shape, e::Event{:mousebutton_down})
-    if inside(Gloria.transform(self.shape, self.x, self.y, self.θ), Point(tocoordinates(shape_layer, e.x, e.y)...), 90.) && isbutton(e, "left")
-        self.active = true
+    if isbutton(e, "left")
+        # moved = self.shape |> rotate(self.θ) |> translate(self.x, self.y)
+        # vs = vertices(moved)
+        # xmin, xmax = extrema(v[1] for v in vs)
+        # ymin, ymax = extrema(v[2] for v in vs)
+        # c = centroid(moved)
+        # mp = Vertex(tocoordinates(shape_layer, e.x, e.y)...)
+        # v = normalize(c .- mp)
+        # p2 = Vertex((mp .+ 1e6 * v)...)
+        # @info "---------------------------"
+        # @info typeof(self.shape)
+        # @info "Shape center: $c"
+        # @info "Boundary: [$xmin, $ymin] x [$xmax, $ymax]"
+        # @info "Mouse position: $mp"
+        # @info "Direction: $v"
+        # @info "Ray target: $p2"
+        # @info "---------------------------"
+        if inside(
+                self.shape |> rotate(self.θ) |> translate(self.x, self.y),
+                Vertex(tocoordinates(shape_layer, e.x, e.y)...)
+            )
+            self.active = true
+        end
     end
 end
 
@@ -58,8 +83,19 @@ end
 # Update
 ##################################################
 
-function update!(self::Shape; t, dt)
-    if any(shape -> inside(Gloria.transform(self.shape, self.x, self.y, self.θ), Gloria.transform(shape.shape, shape.x, shape.y, shape.θ), 90), filter(x->x ≢ self, shape_layer))
+function update!(self::Shape, ::Gloria.AbstractLayer, t, dt)
+    if any(
+            shape -> any(
+                v -> inside(
+                    self.shape |> rotate(self.θ) |> translate(self.x, self.y), v
+                ),
+                vertices(
+                    shape.shape |> rotate(shape.θ) |> translate(shape.x, shape.y)
+                )
+            ),
+            filter(x->x ≢ self, shape_layer)
+        )
+
         self.color = colorant"#FA00FA"
     else
         self.color = colorant"#303030"
@@ -70,7 +106,7 @@ end
 # Render
 ##################################################
 
-function render!(layer::Layer, self::Shape; frame::Int, fps::Float64)
+function render!(layer::Layer, self::Shape, frame::Int, fps::Float64)
     render!(layer, self.shape, self.x, self.y, self.θ, color=self.color)
 end
 
@@ -86,13 +122,13 @@ const controls_layer = Layer([Controls()], show=false)
 
 const shape_layer = Layer(Shape[], width/2, height/2)
 
-const scene = Scene(controls_layer, shape_layer, color=colorant"#D0D0D0")
+const scene = Scene(shape_layer, controls_layer, color=colorant"#D0D0D0")
 const window = Window("Inside", width, height, scene, fullscreen=false)
 const keyboard = Gloria.KeyboardState()
 
-add!(shape_layer, Shape(Circle(0, 0, 100), 0., 0., 0.))
-add!(shape_layer, Shape(Polygon(Point(0, 0), Point(100, 0), Point(0, 100)), 200., 0., 40.))
-add!(shape_layer, Shape(Lines([Point(100(i - 5), randn()*50 + 200) for i in 0:10]...), 0., 0., -10.))
+add!(shape_layer, Shape(circle(Vertex(0., 0.), 100), 0., 0., 0.))
+add!(shape_layer, Shape(Polygon(Vertex(0, 0), Vertex(100, 0), Vertex(0, 100)), 200., 0., 40.))
+add!(shape_layer, Shape(Polyline([Vertex(100(i - 5), randn()*50 + 200) for i in 0:10]...), 0., 0., -10.))
 
 function main(;keepalive=true)
     Gloria.run!(window)
@@ -100,5 +136,6 @@ function main(;keepalive=true)
         wait(window)
     end
 end
+main()
 
 end # module
